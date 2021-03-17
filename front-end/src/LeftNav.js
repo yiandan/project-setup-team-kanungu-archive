547fdc9e32c86931035f68e3b489987d68d6e843
icequeenemily <53096299+icequeenemily@users.noreply.github.com>
2021-03-17 11:46:02 -0400

    start off app; attempt at nav

---
+++ front-end/src/LeftNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LeftNav.css';
+import { NavLink } from 'react-router-dom';
+
+const LeftNav = ({open}) => {
+    return (
+        <ul open={open}>
+            <li> <NavLink to="./"> Home </NavLink></li>
+            <li> <NavLink to="./Search"> Search</NavLink></li>
+        </ul>
+    )
+}
+
+export default LeftNav
---
+++ front-end/src/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import App from './App'
+import './Search.css'
+import Nav from './Nav'
+
+const Search=()=> {
+    return (
+        <div className="container">
+            <h1>Search Page</h1>
+        </div>
+    )
+}
+
+export default Search
