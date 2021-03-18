2985118c77fa351b5e16d730b82593e5a8891e45
icequeenemily <53096299+icequeenemily@users.noreply.github.com>
2021-03-18 16:40:16 -0400

    added to nav and started a search bar

--- front-end/src/Search.js
+++ front-end/src/Search.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 import App from './App'
 import './Search.css'
 import Nav from './Nav'
+import SearchBar from './SearchBar'
 
 const Search=()=> {
     return (
         <div className="container">
             <h1>Search Page</h1>
             <Nav />
+            <SearchBar />
         </div>
     )
 }
