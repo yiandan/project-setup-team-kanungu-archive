28a6c5266aedad7cb6ce7986d4fe8b5adc536f2c
Nancy <nh1416@nyu.edu>
2021-03-21 00:35:48 -0400

    Added forms to PostRecipe page

--- front-end/src/PostNewRecipe.js
+++ front-end/src/PostNewRecipe.js
@@ -8,8 +8,42 @@ import Nav from './Nav'
 const PostNewRecipe=()=> {
     return (
         <div className="container">
-            <h1>Post New Recipe</h1>
-          
+            <h1>Post a New Recipe!</h1>
+            <div className="wrapper">
+             <form>
+             <fieldset>
+               <legend>New Recipe:</legend>
+               <label>
+                 <p>Name of Recipe</p>
+                 <input name="NameOfRecipe" />
+               </label>
+               <label>
+                 <p>Ingredients</p>
+                 <input name="NameOfRecipe" />
+               </label>
+               <label>
+                 <p>Cuisine</p>
+                 <input name="NameOfRecipe" />
+               </label>
+               <label>
+                 <p>Instructions</p>
+                 <input name="NameOfRecipe" />
+               </label>
+               <label>
+                <p> Difficulty </p>
+               </label>
+               <select name="diff" id="diff">
+                 <optgroup>
+                 <option value="Beginner">Beginner</option>
+                 <option value="Intermediate">Intermediate</option>
+                 <option value="Difficult">Difficult</option>
+                 </optgroup>
+                 </select>
+                 </fieldset>
+                <button type="post">Post</button>
+              
+             </form>
+        </div>
         </div>
     )
 }
