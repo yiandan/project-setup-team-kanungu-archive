e16e7a51b8f1d8aef8731663f12aac1b54a5fac6
mv1572 <distractionfree@Matthews-MacBook-Pro.local>
2021-03-23 18:50:29 -0400

    Changes text when expanding a recipe (instead of just collapse)

--- front-end/src/RecipePage.js
+++ front-end/src/RecipePage.js
@@ -25,7 +25,25 @@ const RecipePage=()=> {
                 </div>
                 <div className = 'car'><ImageSlider images={[food1,food2,food3]}/></div>
                 <div className = 'Text'>
-                <i>Lorem ipsum dolor sit amet, consectetur<br></br> adipiscing elit. Fusce vitae sapien malesuada<br></br>, fermentum orci id, commodo nulla. Quisque erat turpis<br></br>, mollis id sem quis, posuere dictum orci.<br></br> Nullam semper nunc nec dui porta iaculis.<br></br> Maecenas quis placerat ipsum, eget<br></br> consectetur erat. Vivamus ultrices odio nunc,<br></br> a luctus orci bibendum non. Interdum et<br></br> malesuada fames ac ante ipsum primis in faucibus.<br></br> Fusce gravida tincidunt magna a facilisis.<br></br> Donec dignissim vulputate efficitur.<br></br> Nulla tristique bibendum ipsum sed bibendum.<br></br> Nulla in vehicula lacus. Donec bibendum diam a purus aliquam,<br></br> vitae iaculis orci viverra. Sed maximus non dolor<br></br> eu consectetur. Curabitur id urna accumsan, ornare ante quis,<br></br> mattis massa. Cras ex eros, varius ut lacinia sit amet,<br></br> accumsan mollis sem. Morbi id nulla rhoncus,<br></br> dictum dolor eu, tempus turpis. Praesent bibendum nibh velit,<br></br> vel laoreet quam rutrum quis.
+                <i>Lorem ipsum dolor sit amet, consectetur<br>
+                </br> adipiscing elit. Fusce vitae sapien malesuada<br>
+                </br>, fermentum orci id, commodo nulla. Quisque erat turpis<br>
+                </br>, mollis id sem quis, posuere dictum orci.<br>
+                </br> Nullam semper nunc nec dui porta iaculis.<br>
+                </br> Maecenas quis placerat ipsum, eget<br>
+                </br> consectetur erat. Vivamus ultrices odio nunc,<br>
+                </br> a luctus orci bibendum non. Interdum et<br>
+                </br> malesuada fames ac ante ipsum primis in faucibus.<br>
+                </br> Fusce gravida tincidunt magna a facilisis.<br>
+                </br> Donec dignissim vulputate efficitur.<br>
+                </br> Nulla tristique bibendum ipsum sed bibendum.<br
+                ></br> Nulla in vehicula lacus. Donec bibendum diam a purus aliquam,<br>
+                </br> vitae iaculis orci viverra. Sed maximus non dolor<br>
+                </br> eu consectetur. Curabitur id urna accumsan, ornare ante quis,<br>
+                </br> mattis massa. Cras ex eros, varius ut lacinia sit amet,<br>
+                </br> accumsan mollis sem. Morbi id nulla rhoncus,<br>
+                </br> dictum dolor eu, tempus turpis. Praesent bibendum nibh velit,<br>
+                </br> vel laoreet quam rutrum quis.
                
 </i>
 </div>
