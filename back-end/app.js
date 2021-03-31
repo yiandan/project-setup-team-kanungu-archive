770ccf6ec73bce4548fcf49b9be22d9881040d90
Allyson Chan <abc9295@nyu.edu>
2021-03-31 17:24:25 -0400

    connected front to back end

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,13 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
+const cors = require("cors")
 // we will put some server logic here later...
 // export the express app we created to make it available to other modules
 module.exports = app
 
+app.use(cors())
+
 app.get("/", (req, res) => {
-    res.send("Hello!")
+    res.send({ message: "We did it!" })
   })
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -17,29 +17,20 @@ import Nav from './Nav'
 import Sidebar from './Sidebar'
 import Login from './Login'
 import SignUp from './SignUp';
-
+import Axios from "axios";
 
 function App() {
+  Axios({
+    method: "GET",
+    url: "http://localhost:5000/",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => {
+    console.log(res.data.message);
+  });
+
   return (
-    /* Reference
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  */
- 
  <div className="App" id="inner-container">
    <div className="App" id="outer-container">
    <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
