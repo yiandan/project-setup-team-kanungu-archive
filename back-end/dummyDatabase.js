f1797b9f03e6afb5b92a47ef55e2f85ba8075dc1
mv1572 <distractionfree@matthews-mbp.myfiosgateway.com>
2021-04-01 11:34:52 -0400

    Makes it so that different "routes" are accesible in the back end

---
+++ back-end/dummyDatabase.js
@@ -0,0 +1,6 @@
+let recipes = [
+    { _id: 1, name: "Pizza", ingredients: "cheese, sauce, dough" },
+    { _id: 2, name: "Cookies", ingredients: "dough" },
+    { _id: 3, name: "Ice Cream", ingredients: "vanilla ice cream, chocolate sauce" }
+  ];
+  module.exports = recipes;
