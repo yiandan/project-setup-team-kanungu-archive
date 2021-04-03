c4c5d517bf2fd22c6e6d2392d62b8356680c816c
icequeenemily <53096299+icequeenemily@users.noreply.github.com>
2021-04-03 14:45:01 -0400

    create back-end for search, edit front-end to filter searches and get results from dummy database

--- back-end/routes/recipes.js
+++ back-end/routes/recipes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 let recipes = require("../dummyDatabase");
 
-router.get("/list", async (req, res) => {
+router.get('/list', async (req, res) => {
     try {
       res.status(200).json({
         data: recipes
@@ -16,11 +16,66 @@ router.get("/list", async (req, res) => {
     }
   });
 
+  
   router.get("/list/:id", async (req, res) => {
-    let { id } = req.params;
+    const { id } = req.params;
     id = Number(id);
     try {
       let recipe = recipes.find(recipe => recipe._id === id);
+      res.status(200).json({
+        data: recipe
+      });
+    } catch (err) {
+      res.status(400).json({
+        message: "Some error occured",
+        err
+      });
+    }
+  });
+  
+  //json body instead of all slashes
+  router.get("/list/:category/:difficulty/:search", async (req, res) => {
+    const { category, difficulty, search } = req.params;
+    console.log(search);
+    let recipe;
+    try {
+      //difficulty easy and different categories 
+      if (difficulty=== "easy") {
+        if (category === "cuisine") {
+          recipe = recipes.filter(recipe => recipe.difficulty=== difficulty && recipe.cuisine.includes(search));
+        }
+        else if (category==="ingredient") {
+          recipe = recipes.filter(recipe => recipe.difficulty=== difficulty && recipe.ingredients.includes(search));
+        }
+        else if (category==="name") {
+          recipe = recipes.filter(recipe => recipe.difficulty=== difficulty && recipe.name.includes(search));
+        }
+      }
+      //difficulty intermediate and different categories 
+      else if (difficulty=== "intermediate") {
+        if (category === "cuisine") {
+          recipe = recipes.filter(recipe => recipe.difficulty=== difficulty && recipe.cuisine.includes(search));
+        }
+        else if (category==="ingredient") {
+          recipe = recipes.filter(recipe => recipe.difficulty=== difficulty && recipe.ingredients.includes(search));
+        }
+        else if (category==="name") {
+          recipe = recipes.filter(recipe => recipe.difficulty=== difficulty && recipe.name.includes(search));
+        }
+      }
+      //difficulty advanced and different categories 
+      else if (difficulty=== "advanced") {
+        if (category === "cuisine") {
+          recipe = recipes.filter(recipe => recipe.difficulty=== difficulty && recipe.cuisine.includes(search));
+        }
+        else if (category==="ingredient") {
+          recipe = recipes.filter(recipe => recipe.difficulty=== difficulty && recipe.ingredients.includes(search));
+        }
+        else if (category==="name") {
+          recipe = recipes.filter(recipe => recipe.difficulty=== difficulty && recipe.name.includes(search));
+        }
+      }
+      //let recipe = recipes.filter(recipe => recipe.category ===category && recipe.name === search || recipe.ingredients.includes(search)) ;
       res.status(200).json({
         data: recipe
       });
