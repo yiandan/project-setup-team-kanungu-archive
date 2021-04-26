9bceca771d4d7fee16d5f6a6aea382852e69a674
dani <dansjo99@gmail.com>
2021-04-25 22:38:11 -0400

    Comment,like functioanlity. profile page fix

--- front-end/src/LikeButton.js
+++ front-end/src/LikeButton.js
@@ -1,30 +1,52 @@
 import { useState } from 'react';
 import './LikeButton.css'
-function LikeButton(){
-const [count,setCount] = useState(0)
+import axios from 'axios'
+import { Error } from 'mongoose';
+import React, { useEffect } from 'react'
+const LikeButton= (props)=> {
+const data = props.data;
+const [count,setCount] = useState(data.curr)
 const [liked,setLiked] = useState(false)
+useEffect(() => {
+    console.log('the age is changed to ',count);
+    axios({url:`http://localhost:5000/post/${data.recipe}/like`,data:{likes:count},method:"PUT"})
+    .catch((err)=>{
+        console.error(err)
+    })
+     
+  },[count]);//here you tell useEffect what state to watch if you want to watch the changing of a  particular state and here we care about someState
 const handleLike = e => { 
-   
+ 
     //Post was unliked, decrement count
     if(liked == true ){
-        
-        setCount(count-1)
+    
         setLiked(false)
+        setCount(count-1)
+       
         
     }
     else{
         
-        setCount(count+1)
         setLiked(true)
+        setCount(count+1)
+        
+        
     }
+    
+    
+  
+   
+  };
 
-}
+
 const colour = liked ? "red" : "grey"
 return(
-<div>
+    <div>
+    
+    <button className = 'but' onClick={handleLike} style = {{color: colour}}>{count} saves</button>
+    </div>
+    )
 
-<button className = 'but' onClick={handleLike} style = {{color: colour}}>{count} saves</button>
-</div>
-)
-}
+} 
+
 export default LikeButton
--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -13,7 +13,61 @@ import { Form } from 'react-bootstrap';
 
 const Avatar = 'https://picsum.photos/200';
 
-
+const obj = 
+{
+    "images": [
+        "https://picsum.photos/id/237/200/300",
+        "https://picsum.photos/id/207/200/300",
+        "https://picsum.photos/id/203/200/300"
+    ],
+    "ingredients": [
+        "rice",
+        "beans",
+        "sauce",
+        ""
+    ],
+    "instructions": [
+        "do something",
+        "do something else",
+        "finally"
+    ],
+    "comments": [
+        "607efbd3ba150d52c9a5b127"
+    ],
+    "slug": "empanadas",
+    "_id": "607cd8046dced78cc134dffe",
+    "author": {
+        "posts": [
+            "607cd8046dced78cc134dffe",
+            "607d7e1832bad543a85f6655",
+            "607d804597a960443a2972a0"
+        ],
+        "followers": [
+            0
+        ],
+        "following": [
+            90
+        ],
+        "likedPosts": [
+            "607d7af66349bb11aed3d108"
+        ],
+        "slug": "chef99",
+        "_id": "607c9b35c463426a0e56e31b",
+        "username": "chef99",
+        "email": "somebody@gmail.com",
+        "firstName": "Bob",
+        "lastName": "Baker",
+        "bio": "World famous chef",
+        "profileImage": "https://picsum.photos/id/222/200/300",
+        "__v": 2
+    },
+    "title": "Empanadas",
+    "posted": "2020-08-20T04:00:00.000Z",
+    "cuisine": "Mexican",
+    "difficulty": "advanced",
+    "likes": 10,
+    "__v": 1
+}
 //import MyRecipes from './MyRecipes'
 
 
@@ -112,15 +166,15 @@ const Profile=()=> {
             </optionRow>
            
             <row>
-                <FeedPost> <button className="button"><RecipePage/> </button> </FeedPost>
-                <FeedPost> <RecipePage/> </FeedPost>
-                <FeedPost> <RecipePage/> </FeedPost>
+                <FeedPost> <button className="button"><RecipePage recipe ={obj}/> </button> </FeedPost>
+                <FeedPost> <RecipePage recipe = {obj}/> </FeedPost>
+                <FeedPost> <RecipePage recipe = {obj}/> </FeedPost>
             </row>
 
             <row>
-                <FeedPost> <RecipePage/> </FeedPost>
-                <FeedPost> <RecipePage/> </FeedPost>
-                <FeedPost> <RecipePage/> </FeedPost>
+                <FeedPost> <RecipePage recipe = {obj}/> </FeedPost>
+                <FeedPost> <RecipePage recipe = {obj}/> </FeedPost>
+                <FeedPost> <RecipePage recipe = {obj}/> </FeedPost>
             </row>
             <Link to="./MyFeed">
                 <h4>Clike here to view all My Feed</h4>
