f7d2ad98f4a9e00e8b5ad918918ca42f7ba09dc0
icequeenemily <53096299+icequeenemily@users.noreply.github.com>
2021-05-03 11:31:32 -0400

    fix populate in back-end search route, fix front end connections

--- back-end/routes/recipes.js
+++ back-end/routes/recipes.js
@@ -58,17 +58,38 @@ router.get('/list', async (req, res) => {
       //difficulty easy and different categories 
       if (difficulty=== "easy") {
         if (category === "cuisine") {
-          recipe = await Recipe.find({$and: [{difficulty:'easy'},{cuisine : {$regex : new RegExp(search, "i")}}]}).populate("author");
+          recipe = await Recipe.find({$and: [{difficulty:'easy'},{cuisine : {$regex : new RegExp(search, "i")}}]}).populate({
+            path:'comments',
+            populate:{
+                path:'by',
+                model:'User',
+                select:['username']
+            }
+        });
           console.log(recipe);
         }
         
         else if (category==="ingredient") {
-          recipe = await Recipe.find({$and: [{difficulty:'easy'},{ingredients : {$regex : new RegExp(search, "i")}}]}).populate("author");
+          recipe = await Recipe.find({$and: [{difficulty:'easy'},{ingredients : {$regex : new RegExp(search, "i")}}]}).populate({
+            path:'comments',
+            populate:{
+                path:'by',
+                model:'User',
+                select:['username']
+            }
+        });
           console.log(recipe);
         }
         
         else if (category==="name") {
-          recipe = await Recipe.find({$and: [{difficulty:'easy'},{title : {$regex : new RegExp(search, "i")}}]}).populate("author");
+          recipe = await Recipe.find({$and: [{difficulty:'easy'},{title : {$regex : new RegExp(search, "i")}}]}).populate({
+            path:'comments',
+            populate:{
+                path:'by',
+                model:'User',
+                select:['username']
+            }
+        });
           console.log(recipe);
         }
       }
@@ -76,31 +97,73 @@ router.get('/list', async (req, res) => {
       //difficulty intermediate and different categories 
       else if (difficulty=== "intermediate") {
         if (category === "cuisine") {
-          recipe = await Recipe.find({$and: [{difficulty:'intermediate'},{cuisine : {$regex : new RegExp(search, "i")}}]}).populate("author");
+          recipe = await Recipe.find({$and: [{difficulty:'intermediate'},{cuisine : {$regex : new RegExp(search, "i")}}]}).populate({
+            path:'comments',
+            populate:{
+                path:'by',
+                model:'User',
+                select:['username']
+            }
+        });
           console.log(recipe);
         }
         
         else if (category==="ingredient") {
-          recipe = await Recipe.find({$and: [{difficulty:'intermediate'},{ingredients : {$regex : new RegExp(search, "i")}}]}).populate("author");
+          recipe = await Recipe.find({$and: [{difficulty:'intermediate'},{ingredients : {$regex : new RegExp(search, "i")}}]}).populate({
+            path:'comments',
+            populate:{
+                path:'by',
+                model:'User',
+                select:['username']
+            }
+        });
           console.log(recipe);
         }
         else if (category==="name") {
-          recipe = await Recipe.find({$and: [{difficulty:'intermediate'},{title : {$regex : new RegExp(search, "i")}}]}).populate("author");
+          recipe = await Recipe.find({$and: [{difficulty:'intermediate'},{title : {$regex : new RegExp(search, "i")}}]}).populate({
+            path:'comments',
+            populate:{
+                path:'by',
+                model:'User',
+                select:['username']
+            }
+        });
           console.log(recipe);
         }
       }
       //difficulty advanced and different categories 
       else if (difficulty=== "advanced") {
         if (category === "cuisine") {
-          recipe = await Recipe.find({$and: [{difficulty:'advanced'},{cuisine : {$regex : new RegExp(search, "i")}}]}).populate("author");
+          recipe = await Recipe.find({$and: [{difficulty:'advanced'},{cuisine : {$regex : new RegExp(search, "i")}}]}).populate({
+            path:'comments',
+            populate:{
+                path:'by',
+                model:'User',
+                select:['username']
+            }
+        });
           console.log(recipe);
         }
         else if (category==="ingredient") {
-          recipe = await Recipe.find({$and: [{difficulty:'advanced'},{ingredients : {$regex : new RegExp(search, "i")}}]}).populate("author");
+          recipe = await Recipe.find({$and: [{difficulty:'advanced'},{ingredients : {$regex : new RegExp(search, "i")}}]}).populate({
+            path:'comments',
+            populate:{
+                path:'by',
+                model:'User',
+                select:['username']
+            }
+        });
           console.log(recipe);
         }
         else if (category==="name") {
-          recipe = await Recipe.find({$and: [{difficulty:'advanced'},{title : {$regex : new RegExp(search, "i")}}]}).populate("author");
+          recipe = await Recipe.find({$and: [{difficulty:'advanced'},{title : {$regex : new RegExp(search, "i")}}]}).populate({
+            path:'comments',
+            populate:{
+                path:'by',
+                model:'User',
+                select:['username']
+            }
+        });
           console.log(recipe);       
         }
       }
